89aa05785f0706fdb03ece600d6b4a42bf7cec00
Aswin M A <ma.aswinashokan@gmail.com>
2023-12-20 12:33:52 +0530

    Complete Project is uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
+import BookingPage from './pages/BookingPage'
+import ConfirmedBooking from './pages/ConfirmedBooking'
+import './App.css'
+import {
+  Route,
+  Routes
+} from "react-router-dom"
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Header />
+     <Routes> 
+      <Route path="/" exact element={<HomePage />}></Route>
+      <Route path="/booking" element={<BookingPage />}></Route> 
+      <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
+    </Routes>
+     <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
